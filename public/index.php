3935aa654d30ce202e56c75fa32ed2036d3c513f
FaustinGarreau <fgarreau@edenschool.fr>
2020-02-05 17:04:02 +0100

    composer install

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 // CHEMAIN
     define('SRC', __DIR__ . '/../src/');
     define('CONTROLLER', __DIR__ . '/../src/Controller/');
@@ -8,7 +9,7 @@
 // ACCES BDD
 
     define('USER', 'root');
-    define('DATABASE', 'objetuser');
+    define('DATABASE', 'innodash');
     define('PASSWORD', 'root');
     $bdd = new PDO('mysql:host=127.0.0.1;dbname=' . DATABASE . ';charset=utf8', USER, PASSWORD);
 
